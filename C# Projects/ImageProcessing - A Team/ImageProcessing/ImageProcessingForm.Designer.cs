ee7833313bf01009e570946a8debd511e765f9c5
Hdtran89 <hdtran89@yahoo.com>
2015-03-03 02:37:26 -0600

    fixed merge

---
+++ C# Projects/ImageProcessing - A Team/ImageProcessing/ImageProcessingForm.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace ImageProcessing
+{
+    partial class ImageProcessingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.runButton = new System.Windows.Forms.Button();
+            this.frameRateNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.imageWidthTextBox = new System.Windows.Forms.TextBox();
+            this.unitsLabel = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.formControlsGroupBox = new System.Windows.Forms.GroupBox();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.widthHeaderLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rangeHeaderLabel = new System.Windows.Forms.Label();
+            this.frameRateHeaderLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.frameRateNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.formControlsGroupBox.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(64, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(521, 212);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(61, 271);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(98, 13);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "Processed: 20/500";
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(22, 40);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 36);
+            this.loadButton.TabIndex = 2;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(459, 40);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 36);
+            this.runButton.TabIndex = 3;
+            this.runButton.Text = "Run";
+            this.runButton.UseVisualStyleBackColor = true;
+            // 
+            // frameRateNumericUpDown
+            // 
+            this.frameRateNumericUpDown.Location = new System.Drawing.Point(352, 52);
+            this.frameRateNumericUpDown.Name = "frameRateNumericUpDown";
+            this.frameRateNumericUpDown.Size = new System.Drawing.Size(82, 20);
+            this.frameRateNumericUpDown.TabIndex = 4;
+            // 
+            // imageWidthTextBox
+            // 
+            this.imageWidthTextBox.Location = new System.Drawing.Point(242, 52);
+            this.imageWidthTextBox.Name = "imageWidthTextBox";
+            this.imageWidthTextBox.Size = new System.Drawing.Size(55, 20);
+            this.imageWidthTextBox.TabIndex = 5;
+            // 
+            // unitsLabel
+            // 
+            this.unitsLabel.AutoSize = true;
+            this.unitsLabel.Location = new System.Drawing.Point(303, 59);
+            this.unitsLabel.Name = "unitsLabel";
+            this.unitsLabel.Size = new System.Drawing.Size(21, 13);
+            this.unitsLabel.TabIndex = 6;
+            this.unitsLabel.Text = "cm";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(121, 52);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(82, 20);
+            this.numericUpDown1.TabIndex = 7;
+            // 
+            // formControlsGroupBox
+            // 
+            this.formControlsGroupBox.Controls.Add(this.frameRateHeaderLabel);
+            this.formControlsGroupBox.Controls.Add(this.rangeHeaderLabel);
+            this.formControlsGroupBox.Controls.Add(this.label2);
+            this.formControlsGroupBox.Controls.Add(this.widthHeaderLabel);
+            this.formControlsGroupBox.Controls.Add(this.pauseButton);
+            this.formControlsGroupBox.Controls.Add(this.loadButton);
+            this.formControlsGroupBox.Controls.Add(this.runButton);
+            this.formControlsGroupBox.Controls.Add(this.numericUpDown1);
+            this.formControlsGroupBox.Controls.Add(this.frameRateNumericUpDown);
+            this.formControlsGroupBox.Controls.Add(this.unitsLabel);
+            this.formControlsGroupBox.Controls.Add(this.imageWidthTextBox);
+            this.formControlsGroupBox.Location = new System.Drawing.Point(21, 300);
+            this.formControlsGroupBox.Name = "formControlsGroupBox";
+            this.formControlsGroupBox.Size = new System.Drawing.Size(607, 100);
+            this.formControlsGroupBox.TabIndex = 8;
+            this.formControlsGroupBox.TabStop = false;
+            this.formControlsGroupBox.Text = "Controls";
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pauseButton.Location = new System.Drawing.Point(551, 47);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(34, 23);
+            this.pauseButton.TabIndex = 11;
+            this.pauseButton.Text = "| |";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(190, 264);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(395, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.imageToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(665, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            // 
+            // imageToolStripMenuItem
+            // 
+            this.imageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runToolStripMenuItem});
+            this.imageToolStripMenuItem.Name = "imageToolStripMenuItem";
+            this.imageToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.imageToolStripMenuItem.Text = "Image";
+            // 
+            // runToolStripMenuItem
+            // 
+            this.runToolStripMenuItem.Name = "runToolStripMenuItem";
+            this.runToolStripMenuItem.Size = new System.Drawing.Size(95, 22);
+            this.runToolStripMenuItem.Text = "Run";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // widthHeaderLabel
+            // 
+            this.widthHeaderLabel.AutoSize = true;
+            this.widthHeaderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.widthHeaderLabel.Location = new System.Drawing.Point(251, 30);
+            this.widthHeaderLabel.Name = "widthHeaderLabel";
+            this.widthHeaderLabel.Size = new System.Drawing.Size(35, 13);
+            this.widthHeaderLabel.TabIndex = 12;
+            this.widthHeaderLabel.Text = "Width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(142, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 13;
+            // 
+            // rangeHeaderLabel
+            // 
+            this.rangeHeaderLabel.AutoSize = true;
+            this.rangeHeaderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rangeHeaderLabel.Location = new System.Drawing.Point(370, 30);
+            this.rangeHeaderLabel.Name = "rangeHeaderLabel";
+            this.rangeHeaderLabel.Size = new System.Drawing.Size(39, 13);
+            this.rangeHeaderLabel.TabIndex = 14;
+            this.rangeHeaderLabel.Text = "Range";
+            // 
+            // frameRateHeaderLabel
+            // 
+            this.frameRateHeaderLabel.AutoSize = true;
+            this.frameRateHeaderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.frameRateHeaderLabel.Location = new System.Drawing.Point(130, 30);
+            this.frameRateHeaderLabel.Name = "frameRateHeaderLabel";
+            this.frameRateHeaderLabel.Size = new System.Drawing.Size(62, 13);
+            this.frameRateHeaderLabel.TabIndex = 15;
+            this.frameRateHeaderLabel.Text = "Frame Rate";
+            // 
+            // ImageProcessingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 421);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.formControlsGroupBox);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ImageProcessingForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.frameRateNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.formControlsGroupBox.ResumeLayout(false);
+            this.formControlsGroupBox.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.NumericUpDown frameRateNumericUpDown;
+        private System.Windows.Forms.TextBox imageWidthTextBox;
+        private System.Windows.Forms.Label unitsLabel;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.GroupBox formControlsGroupBox;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem imageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label frameRateHeaderLabel;
+        private System.Windows.Forms.Label rangeHeaderLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label widthHeaderLabel;
+    }
+}
+
